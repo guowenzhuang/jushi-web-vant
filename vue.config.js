df30163d21484a274031cb8f2e53564afbd98848
guowenzhuang <80795289@qq.com>
2019-06-11 08:26:34 +0800

    init files

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,6 @@ module.exports = {
       }
     }
   },
-  publicPath: process.env.NODE_ENV === 'codingme' ? '/jushi-web-vant' : '/'
+  publicPath: process.env.NODE_ENV === 'codingme' ? '/jushi-web-vant/dist' : '/'
 
 }
