17d86e620f8033307426747e633aa8ca8e142c59
腾讯手机管家 <guowenzhuang>
2019-06-11 12:54:49 +0800

    init files

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -9,7 +9,7 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
     HelloWorld
   }
---
+++ src/views/Home/index.js
@@ -0,0 +1,3 @@
+import home from './Home'
+
+export default home
