517d1636fcc75feaa5296145e5cafa8d0c22e50d
腾讯手机管家 <guowenzhuang>
2019-06-12 21:11:50 +0800

    f

--- src/views/Me/Me.vue
+++ src/views/Me/Me.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="me">
-    我的
+  <div id="logincss">
+  <van-button  type="default" @click="login()">登录</van-button>
   </div>
+ </div>
 </template>
 
 <script>
   export default {
-    name: 'Me'
-  }
+    name: 'Me',
+     data(){
+       return {
+
+       }
+     },
+     methods:{
+       login(){
+         alert("登录中")
+       }
+     }
+    }
+     
+ 
+  
 </script>
 
 <style scoped>
-
+           
+.logincss{
+ padding-top: 100px;
+ margin-top: 1000px;
+}
 </style>
