80143b2f9b82f6c0141b857cff068b39a7544c74
腾讯手机管家 <guowenzhuang>
2019-06-12 21:05:47 +0800

    sd

--- src/plugin/vant/index.js
+++ src/plugin/vant/index.js
@@ -1,5 +1,9 @@
 import {Button, Tabbar, TabbarItem} from 'vant'
 import Vue from 'vue'
+import { Popup } from 'vant';
+import { Cell, CellGroup } from 'vant';
 
+Vue.use(Cell).use(CellGroup);
+Vue.use(Popup);
 Vue.use(Button)
 Vue.use(Tabbar).use(TabbarItem)
