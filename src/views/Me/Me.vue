2582202d4d7af06806cd41fdb248edeca0e87352
80795289@qq.com <qq123456>
2019-06-27 10:14:26 +0800

    sd

--- src/views/Me/Me.vue
+++ src/views/Me/Me.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="me">
-    <van-tabs v-model="van_tabs_active" type="card" sticky color="#1296db">
+    <van-tabs type="card" sticky color="#1296db">
       <van-tab title="我">
-    <van-list>
-      <van-cell is-link>
-        <template slot="default">
-          <div class="login_list" @click="login()">
-            <font-awesome-icon icon="user-secret" size="lg" />
-             <van-button style="margin-left: 20px;border: 0px;font-size: 18px;" >
-                登录/注册
-             </van-button>
-          </div>
-        </template>
-      </van-cell>
-    </van-list>
+        <van-list>
+          <van-cell is-link>
+            <template slot="default">
+              <div class="login_list" @click="login()">
+                <font-awesome-icon icon="user-secret" size="lg"/>
+                <van-button style="margin-left: 20px;border: 0px;font-size: 18px;">
+                  登录/注册
+                </van-button>
+              </div>
+            </template>
+          </van-cell>
+        </van-list>
       </van-tab>
     </van-tabs>
   </div>
@@ -23,24 +23,24 @@
   export default {
     name: 'Me',
     data () {
-      return {
-        van_tabs_active: 0
-      }
+      return {}
     },
     methods: {
-      login(){
-     this.$router.push('/login')}
+      login () {
+        this.$router.push('/login')
+      }
     }
   }
 </script>
 
 <style lang="scss">
-  #me{
+  #me {
     .login_list {
       display: flex;
       align-items: center;
     }
-    .van-tabs__nav--card{
+
+    .van-tabs__nav--card {
       margin: 0;
     }
   }
